fce7263b91641cff2081332fcd8c93b58af21ce7
jmcmicha <jmcmicha@com1636.eecs.utk.edu>
2022-01-26 11:48:17 -0500

    done?

--- mouse_frame_control.c
+++ mouse_frame_control.c
@@ -10,8 +10,9 @@ static int                mouse_loc_r;
 static int                mouse_loc_c;
 static int                no_more;
 static int                on_boarder;
+static int                on_bottom_right;
 static yed_direct_draw_t *dd;
-static yed_frame         *left_frame;
+static yed_frame_tree    *left_tree;
 
 static void       mouse(yed_event* event);
 static void       mouse_unload(yed_plugin *self);
@@ -25,7 +26,6 @@ static void       middle_drag(yed_event* event);
 static void       middle_release(yed_event* event);
 static void       draw_corner(yed_event* event);
 static void       draw(void);
-static void       check_for_left(yed_frame_tree *frame_tree, void *event);
 
 int yed_plugin_boot(yed_plugin *self) {
     yed_plugin_request_mouse_reporting(self);
@@ -38,19 +38,21 @@ int yed_plugin_boot(yed_plugin *self) {
     yed_plugin_add_event_handler(self, mouse_eh);
 
     yed_event_handler corner_eh;
-    corner_eh.kind = EVENT_FRAME_ACTIVATED;
+    corner_eh.kind = EVENT_PRE_DIRECT_DRAWS;
     corner_eh.fn   = draw_corner;
     yed_plugin_add_event_handler(self, corner_eh);
 
     yed_plugin_set_unload_fn(self, mouse_unload);
 
-    box_h                = 5;
-    box_w                = 5;
+    box_h                = 2;
+    box_w                = 4;
     mouse_loc_r          = 0;
     mouse_loc_c          = 0;
     if_dragging_resize   = 0;
     if_dragging_resize_b = 0;
     if_dragging_move     = 0;
+    on_bottom_right      = 0;
+    on_boarder           = 0;
     no_more              = 0;
 
     return 0;
@@ -70,9 +72,9 @@ int yed_cell_is_in_frame_tree_mouse(int row, int col, yed_frame_tree *frame_tree
     yed_frame_tree_get_absolute_rect(frame_tree, &top, &left, &height, &width);
 
     return (row >= (top * (ys->term_rows -2))                                   &&
-            row <= (top * (ys->term_rows -2)) + (height * ys->term_rows -2) - 1 &&
+            row <= (top * (ys->term_rows -2)) + (height * ys->term_rows -2) &&
             col >= (left * ys->term_cols)                                       &&
-            col <= (left * ys->term_cols) + (width * ys->term_cols) - 1);
+            col <= (left * ys->term_cols) + (width * ys->term_cols));
 }
 
 //see if cursor is inside of active frame
@@ -104,11 +106,18 @@ static int is_in_bottom_right(yed_frame* frame, yed_event* event) {
 
     frame_tree = yed_frame_tree_get_root(frame->tree);
     yed_frame_tree_get_absolute_rect(frame_tree, &top, &left, &height, &width);
+/*     LOG_FN_ENTER(); */
+/*     yed_log("bottom_right\n"); */
+/*     yed_log("current_row:%3d <= frame->top:%7.3f\n", MOUSE_ROW(event->key), roundf((height + top) * (ys->term_rows - 2))); */
+/*     yed_log("current_row:%3d >= frame->bottom:%7.3f\n", MOUSE_ROW(event->key), roundf((height + top) * (ys->term_rows - 2)) - box_h); */
+/*     yed_log("current_col:%3d <= frame->left:%7.3f\n", MOUSE_COL(event->key), roundf((width + left) * ys->term_cols)); */
+/*     yed_log("current_col:%3d >= frame->right:%7.3f\n", MOUSE_COL(event->key), roundf((width + left) * ys->term_cols) - box_w); */
+/*     LOG_EXIT(); */
 
-    if (MOUSE_ROW(event->key) <= (height * (ys->term_rows - 2)) + (top * (ys->term_rows - 2))         &&
-        MOUSE_ROW(event->key) >= (height * (ys->term_rows - 2)) + (top * (ys->term_rows - 2)) - box_h &&
-        MOUSE_COL(event->key) <= (width * ys->term_cols) + (left * ys->term_cols)                     &&
-        MOUSE_COL(event->key) >= (width * ys->term_cols) + (left * ys->term_cols) - box_w) {
+    if (MOUSE_ROW(event->key) <= roundf((height + top) * (ys->term_rows - 2))         &&
+        MOUSE_ROW(event->key) >= roundf((height + top) * (ys->term_rows - 2)) - box_h &&
+        MOUSE_COL(event->key) <= roundf((width + left) * ys->term_cols)               &&
+        MOUSE_COL(event->key) >= roundf((width + left) * ys->term_cols) - box_w) {
         return 1;
     }else{
         return 0;
@@ -129,10 +138,10 @@ static int is_on_boarder(yed_frame* frame, yed_event* event) {
     yed_frame_tree_get_absolute_rect(frame_tree, &top, &left, &height, &width);
 
 
-    if ((MOUSE_ROW(event->key) <= MAX(1, (top * (ys->term_rows - 2))))        ||   //top
-        (MOUSE_ROW(event->key) >= ((height + top) * (ys->term_rows - 2)) - 1) ||   //bottom
-        (MOUSE_COL(event->key) <= MAX(1, (left * ys->term_cols)))             ||   //left
-        (MOUSE_COL(event->key) >= ((width + left) * ys->term_cols) - 1)) {         //right
+    if ((MOUSE_ROW(event->key) <= MAX(1, roundf(top * (ys->term_rows - 2))))        ||   //top
+        (MOUSE_ROW(event->key) >= roundf((height + top) * (ys->term_rows - 2))) ||   //bottom
+        (MOUSE_COL(event->key) <= MAX(1, roundf(left * ys->term_cols)))             ||   //left
+        (MOUSE_COL(event->key) >= roundf((width + left) * ys->term_cols))) {         //right
         return 0;
     }
 
@@ -160,8 +169,27 @@ static int is_on_boarder(yed_frame* frame, yed_event* event) {
 }
 
 static void middle_click(yed_event* event) {
-    yed_frame *frame;
+    float           top;
+    float           left;
+    float           width;
+    float           height;
+    yed_frame      *frame;
     yed_frame_tree *frame_tree;
+
+    yed_frame_tree **frame_tree_it;
+/*     int loc = 0; */
+/*     LOG_FN_ENTER(); */
+/*     array_traverse(ys->frame_trees, frame_tree_it) { */
+/*         yed_frame_tree_get_absolute_rect((*frame_tree_it), &top, &left, &height, &width); */
+/*         yed_log("    \n"); */
+/*         yed_log("Frame:%d\n", loc); */
+/*         yed_log("current_row:%3d >    frame->top:%7.3f     atop:%5.3f\n", MOUSE_ROW(event->key), MAX(1, roundf(top * (ys->term_rows - 2))), top); */
+/*         yed_log("current_row:%3d < frame->bottom:%7.3f  aheight:%5.3f\n", MOUSE_ROW(event->key), (roundf((height + top) * (ys->term_rows - 2))), height); */
+/*         yed_log("current_col:%3d >   frame->left:%7.3f    aleft:%5.3f\n", MOUSE_COL(event->key), MAX(1, roundf(left * ys->term_cols)), left); */
+/*         yed_log("current_col:%3d <  frame->right:%7.3f   awidth:%5.3f\n", MOUSE_COL(event->key), (roundf((width + left) * ys->term_cols)), width); */
+/*         loc++; */
+/*     } */
+/*     LOG_EXIT(); */
 
     frame = find_frame(event);
     if (frame == NULL) {
@@ -171,108 +199,46 @@ static void middle_click(yed_event* event) {
 
     mouse_loc_r = MOUSE_ROW(event->key);
     mouse_loc_c = MOUSE_COL(event->key);
-    left_frame  = NULL;
 
+    on_bottom_right = is_in_bottom_right(frame, event);
     on_boarder = is_on_boarder(frame, event);
-    LOG_FN_ENTER();
+    left_tree = NULL;
     if (on_boarder) {
-        if (frame->tree->parent != NULL) {
-/*             if (frame->tree->parent->child_trees[0] == frame->tree) { */
-/*                 left_frame = frame; */
-/*             }else{ */
-                frame_tree = frame->tree;
-                if (on_boarder == 1) {
-                    BACK:;
-                    while (frame_tree->parent != NULL && frame_tree->parent->split_kind != FTREE_HSPLIT) {
-                        frame_tree = frame_tree->parent;
-                    }
-                    yed_frame_tree_leaves_do(frame_tree->parent->child_trees[0], check_for_left, event);
-                    if (left_frame == NULL) {
-                        if (frame_tree->parent != NULL) {
-                            frame_tree = frame_tree->parent;
-                            goto BACK;
-                        }
-                    }
-                }else if (on_boarder == 2) {
-                    while (frame_tree->parent != NULL && frame_tree->parent->split_kind != FTREE_VSPLIT) {
-                        frame_tree = frame_tree->parent;
-                    }
-                    yed_frame_tree_leaves_do(frame_tree->parent->child_trees[0], check_for_left, event);
+/*         LOG_FN_ENTER(); */
+/*         yed_log("on_border:%d\n", on_boarder); */
+/*         LOG_EXIT(); */
+        frame_tree = frame->tree;
+        while (frame_tree->parent != NULL) {
+            if ((on_boarder == 1 && frame_tree->parent->split_kind == FTREE_HSPLIT) ||
+                (on_boarder == 2 && frame_tree->parent->split_kind == FTREE_VSPLIT)) {
+                yed_frame_tree_get_absolute_rect(frame_tree->parent, &top, &left, &height, &width);
+/*                 LOG_FN_ENTER(); */
+/*                 yed_log("Check\n"); */
+/*                 yed_log("current_row:%3d >    frame->top:%3.1f\n", MOUSE_ROW(event->key), MAX(1, roundf(top * (ys->term_rows - 2)))); */
+/*                 yed_log("current_row:%3d < frame->bottom:%3.1f\n", MOUSE_ROW(event->key), roundf((height + top) * (ys->term_rows - 2))-1); */
+/*                 yed_log("current_col:%3d >   frame->left:%3.1f\n", MOUSE_COL(event->key), MAX(1, roundf(left * ys->term_cols))); */
+/*                 yed_log("current_col:%3d <  frame->right:%3.1f\n", MOUSE_COL(event->key), roundf((width + left) * ys->term_cols)-1); */
+/*                 LOG_EXIT(); */
+                if ((MOUSE_ROW(event->key) > MAX(1, roundf(top * (ys->term_rows - 2))))        &&   //top
+                    (MOUSE_ROW(event->key) < roundf((height + top) * (ys->term_rows - 2))-1) &&   //bottom
+                    (MOUSE_COL(event->key) > MAX(1, roundf(left * ys->term_cols)))             &&   //left
+                    (MOUSE_COL(event->key) < roundf((width + left) * ys->term_cols)-1)) {         //right
+/*                     LOG_FN_ENTER(); */
+/*                     yed_log("woo"); */
+/*                     LOG_EXIT(); */
+                    left_tree = frame_tree->parent->child_trees[0];
+                    break;
                 }
-/*             } */
+            }
+            frame_tree = frame_tree->parent;
+        }
+        if (frame_tree->parent != NULL) {
+            left_tree = frame_tree->parent->child_trees[0];
+        }else{
+            left_tree = frame_tree->child_trees[0];
         }
     }
-    LOG_EXIT();
-
     event->cancel = 1;
-}
-
-static void check_for_left(yed_frame_tree *frame_tree, void *event) {
-    float           top;
-    float           left;
-    float           width;
-    float           height;
-    yed_event      *event1;
-    yed_frame      *frame;
-
-    event1 = event;
-    frame  = frame_tree->frame;
-    yed_frame_tree_get_absolute_rect(yed_frame_tree_get_root(frame_tree), &top, &left, &height, &width);
-
-    LOG_FN_ENTER();
-    yed_log("Check\n");
-    yed_log("current_row:%d  =           top:%d\n", MOUSE_ROW(event1->key), frame->btop);
-    yed_log("current_row:%d  =        bottom:%d\n", MOUSE_ROW(event1->key), frame->btop + frame->bheight - 1);
-    yed_log("current_row:%d <=    frame->top:%f\n", MOUSE_ROW(event1->key), MAX(1, (top * (ys->term_rows - 2))));
-    yed_log("current_row:%d >= frame->bottom:%f\n", MOUSE_ROW(event1->key), ((height + top) * (ys->term_rows - 2) - 1));
-    yed_log("current_col:%d  =          left:%d\n", MOUSE_COL(event1->key), frame->bleft);
-    yed_log("current_col:%d  =         right:%d\n", MOUSE_COL(event1->key), frame->bleft + frame->bwidth - 1);
-    yed_log("current_col:%d <=   frame->left:%f\n", MOUSE_COL(event1->key), MAX(1, (left * ys->term_cols)));
-    yed_log("current_col:%d >=  frame->right:%f\n", MOUSE_COL(event1->key), ((width + left) * ys->term_cols - 1));
-    LOG_EXIT();
-
-    LOG_FN_ENTER();
-    if ((MOUSE_ROW(event1->key) <= MAX(1, (top * (ys->term_rows - 2))))        ||   //top
-        (MOUSE_ROW(event1->key) >= ((height + top) * (ys->term_rows - 2)) - 1) ||   //bottom
-        (MOUSE_COL(event1->key) <= MAX(1, (left * ys->term_cols)))             ||   //left
-        (MOUSE_COL(event1->key) >= ((width + left) * ys->term_cols) - 1)) {         //right
-        yed_log("outer edge\n");
-        LOG_EXIT();
-        return;
-    }
-
-    if (MOUSE_ROW(event1->key) ==  frame->btop                        ||
-        MOUSE_ROW(event1->key) == (frame->btop + frame->bheight - 1)) {
-            if (MOUSE_COL(event1->key) >= frame->bleft && MOUSE_COL(event1->key) <= (frame->bleft + frame->bwidth - 1)) {
-                yed_log("on top/bottom within left/right\n");
-                goto LEFT;
-            }else{
-                yed_log("on top/bottom not within left/right\n");
-                LOG_EXIT();
-                return;
-            }
-    }else if (MOUSE_COL(event1->key) ==  frame->bleft                       ||
-              MOUSE_COL(event1->key) == (frame->bleft + frame->bwidth - 1)) {
-            if (MOUSE_ROW(event1->key) >= frame->btop && MOUSE_ROW(event1->key) <= (frame->btop + frame->bheight - 1)) {
-                yed_log("on left/right within top/bottom\n");
-                goto LEFT;
-            }else{
-                yed_log("on left/right not within top/bottom\n");
-                LOG_EXIT();
-                return;
-            }
-    }else{
-        yed_log("not on left/right or top/bottom\n");
-        LOG_EXIT();
-        return;
-    }
-
-    LEFT:;
-    if (frame_tree == frame_tree->parent->child_trees[0]) {
-        yed_log("set left_frame\n");
-        LOG_EXIT();
-        left_frame = frame_tree->frame;
-    }
 }
 
 static void middle_drag(yed_event* event) {
@@ -287,49 +253,43 @@ static void middle_drag(yed_event* event) {
 
     frame = ys->active_frame;
 
-    if (if_dragging_resize || is_in_bottom_right(frame, event)) {
-/*         resize frame */
+    if (if_dragging_resize || on_bottom_right) {
         if (if_dragging_resize == 0) {
             if_dragging_resize = 1;
             mouse_loc_r = MOUSE_ROW(event->key);
             mouse_loc_c = MOUSE_COL(event->key);
         }else {
-            if (yed_frame_tree_is_root(frame->tree)) {
-                yed_resize_frame(frame, MOUSE_ROW(event->key) - mouse_loc_r, MOUSE_COL(event->key) - mouse_loc_c);
-            }else {
-                frame_tree = yed_frame_tree_get_root(frame->tree);
-                frame_tree = yed_frame_tree_get_split_leaf_prefer_right_or_bottommost(frame_tree->child_trees[0]);
-                if (frame == frame_tree->frame) {
-                    yed_resize_frame_tree(frame->tree, MOUSE_ROW(event->key) - mouse_loc_r, MOUSE_COL(event->key) - mouse_loc_c);
-                }
-            }
+            frame_tree = yed_frame_tree_get_root(frame->tree);
+            yed_resize_frame_tree(frame_tree, MOUSE_ROW(event->key) - mouse_loc_r, MOUSE_COL(event->key) - mouse_loc_c);
 
             mouse_loc_r = MOUSE_ROW(event->key);
             mouse_loc_c = MOUSE_COL(event->key);
         }
-    }else if (if_dragging_resize_b || (on_boarder && if_dragging_resize_b == 0)) {
+    } if (if_dragging_resize_b || (on_boarder && if_dragging_resize_b == 0)) {
         if (if_dragging_resize_b == 0) {
             if_dragging_resize_b = 1;
         }
-
-        if (left_frame != NULL) {
+        if (left_tree != NULL) {
             if (on_boarder == 1) {
-                yed_resize_frame(left_frame, MOUSE_ROW(event->key) - mouse_loc_r, 0);
+                yed_resize_frame_tree(left_tree, MOUSE_ROW(event->key) - mouse_loc_r, 0);
             }else if (on_boarder == 2) {
-                yed_resize_frame(left_frame, 0, MOUSE_COL(event->key) - mouse_loc_c);
+/*                 LOG_FN_ENTER(); */
+/*                 yed_log("vsplit\n"); */
+/*                 LOG_EXIT(); */
+                yed_resize_frame_tree(left_tree, 0, MOUSE_COL(event->key) - mouse_loc_c);
             }
             mouse_loc_r = MOUSE_ROW(event->key);
             mouse_loc_c = MOUSE_COL(event->key);
         }
-    }else if(yed_cell_is_in_frame_tree_mouse(MOUSE_ROW(event->key), MOUSE_COL(event->key), yed_frame_tree_get_root(frame->tree))){
-/*         move frame */
+    } else if(yed_cell_is_in_frame_tree_mouse(MOUSE_ROW(event->key), MOUSE_COL(event->key), yed_frame_tree_get_root(frame->tree))){
+        //move frame
         if (if_dragging_move == 0) {
             if_dragging_move = 1;
             mouse_loc_r = MOUSE_ROW(event->key);
             mouse_loc_c = MOUSE_COL(event->key);
         }else {
-/*             update height */
-/*             MOVE DOWN */
+            //update height
+            //MOVE DOWN
             if (MOUSE_ROW(event->key) > mouse_loc_r) {
                 frame_tree = yed_frame_tree_get_root(frame->tree);
                 yed_frame_tree_get_absolute_rect(frame_tree, &top, &left, &height, &width);
@@ -341,7 +301,7 @@ static void middle_drag(yed_event* event) {
 
                 yed_move_frame(frame, MOUSE_ROW(event->key) - mouse_loc_r, 0);
 
-/*             MOVE UP */
+            //MOVE UP
             }else if (MOUSE_ROW(event->key) < mouse_loc_r) {
                 yed_move_frame(frame, MOUSE_ROW(event->key) - mouse_loc_r, 0);
 
@@ -353,8 +313,8 @@ static void middle_drag(yed_event* event) {
             }
             mouse_loc_r = MOUSE_ROW(event->key);
 
-/*             update width */
-/*             MOVE RIGHT */
+            //update width
+            //MOVE RIGHT
             if (MOUSE_COL(event->key) > mouse_loc_c) {
                 frame_tree = yed_frame_tree_get_root(frame->tree);
                 yed_frame_tree_get_absolute_rect(frame_tree, &top, &left, &height, &width);
@@ -371,7 +331,7 @@ static void middle_drag(yed_event* event) {
                     yed_frame_tree_set_relative_rect(frame_tree, 0.0, 0.5, 1.0, 0.5);
                     yed_frame_tree_recursive_readjust(frame_tree);
                 }
-/*             MOVE LEFT */
+            //MOVE LEFT
             }else if (MOUSE_COL(event->key) < mouse_loc_c) {
                 frame_tree = yed_frame_tree_get_root(frame->tree);
                 yed_frame_tree_get_absolute_rect(frame_tree, &top, &left, &height, &width);
@@ -393,7 +353,6 @@ static void middle_drag(yed_event* event) {
             mouse_loc_c = MOUSE_COL(event->key);
         }
     }
-    draw();
     event->cancel = 1;
 }
 
@@ -404,6 +363,8 @@ static void middle_release(yed_event* event) {
     if_dragging_move     = 0;
     mouse_loc_r          = 0;
     mouse_loc_c          = 0;
+    on_bottom_right      = 0;
+    on_boarder           = 0;
     no_more              = 0;
 }
 
@@ -432,19 +393,29 @@ static void draw_corner(yed_event* event) {
 }
 
 static void draw() {
-    yed_frame* frame;
-
-    frame = ys->active_frame;
-    if (frame == NULL) {return;}
+    yed_frame*      frame;
+    yed_frame_tree *frame_tree;
 
     if (dd != NULL) {
         yed_kill_direct_draw(dd);
     }
+
+    frame = ys->active_frame;
+    if (frame == NULL) {return;}
+
+    frame_tree = yed_frame_tree_get_root(frame->tree);
+    if (!frame_tree->is_leaf) {
+        frame = yed_frame_tree_get_split_leaf_prefer_right_or_bottommost(frame_tree->child_trees[0])->frame;
+    }
+
     dd = yed_direct_draw(frame->bheight + frame->btop - 1,
-                         frame->bwidth + frame->bleft - 1,
-                         yed_active_style_get_active(),
-                         "⇲");
+                        frame->bwidth + frame->bleft - 1,
+                        yed_active_style_get_active(),
+                        "⇲");
 }
 
 void mouse_unload(yed_plugin *self) {
+    if (dd != NULL) {
+        yed_kill_direct_draw(dd);
+    }
 }
